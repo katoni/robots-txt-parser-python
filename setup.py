3fe00ead577ca18a001fbb48b70c72268e4816e8
Martin Skovvang Petersen <mpskovvang@outlook.com>
2018-12-18 08:30:09 +0100

    Add original module code (robotexclusionrulesparser 1.7.1)
    https://pypi.org/project/robotexclusionrulesparser/

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="robots-txt-parser",
+    version="1.0.0",
+    author='Philip Semanchuk',
+    author_email='philip@pyspoken.com',
+    maintainer="Martin Skovvang Petersen",
+    maintainer_email="martin@katoni.dk",
+    description="A Robot Exclusion Rules Parser for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    summary='A robots.txt parser alternative to Python\'s urllib.robotparser module',
+    url="https://github.com/katoni/robotexclusionrulesparser",
+    license='BSD',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
